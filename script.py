e98198dd40e75172b8ebed4010a3789cfaec993f
k1n4kut4 <recruitlukejones@gmail.com>
2024-02-05 17:32:45 +0000

    added call to nasdaq api

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 import stock_interface
 
 #Bank of America
-bac = stock_interface.get__timeseries('BAC', 7)
+bac = stock_interface.get__timeseries('BAC', 'nasdaq', 7)
 
-print('--- \nStock: BAC \n--- \nHead:')
-print(bac.head())
-print('\nTail:')
-print(bac.tail())
+print(bac)
+
+# print('--- \nStock: BAC \n--- \nHead:')
+# print(bac.head())
+# print('\nTail:')
+# print(bac.tail())
--- stock_interface.py
+++ stock_interface.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import pandas_datareader as pdr
+import nasdaqdatalink
 import datetime 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
 stock_symbols = ['BAC']
+nasdaqdatalink.ApiConfig.api_key = API_KEY
 
-def get__timeseries_of_stock(stock_name: str, start_date, end_date):
-    return pdr.DataReader(stock_name, 'stooq', start_date, end_date)
+def get__timeseries_of_stock(stock_name: str, source: str, start_date, end_date):
+    if(source=="nasdaq"):
+        stock_name_prefixed = "WIKI/" + stock_name
+        return nasdaqdatalink.get(stock_name_prefixed, start_date=str(start_date), end_date=str(end_date))
+    return pdr.DataReader(stock_name, source, start_date, end_date)
 
-def get__timeseries(stock_name: str, period_back: int):
+def get__timeseries(stock_name: str, source: str, period_back: int):
     if(stock_name is None): 
         return "Invalid 'stock_name' parameter given"
     if(period_back is None): 
@@ -16,4 +25,4 @@ def get__timeseries(stock_name: str, period_back: int):
     date_week_ago = date_today - datetime.timedelta(period_back)
     start_date = date_week_ago
     end_date =  date_today
-    return get__timeseries_of_stock(stock_name, start_date, end_date)
+    return get__timeseries_of_stock(stock_name, source, start_date, end_date)
